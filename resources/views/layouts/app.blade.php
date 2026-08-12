6375bf4952a5af4d60d801f2192e81651682004c
tuyulcode <ubaid7rahman@gmail.com>
2026-08-12 10:57:59 +0700

    update nama web dan logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>@yield('title', 'Dashboard') — e-Rekap Operasional</title>
+  <title>@yield('title', 'Dashboard') — e-Operasional</title>
 
   <!-- Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
